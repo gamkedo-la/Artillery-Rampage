3fe8fec56441e6b1404b2cadc382399baef942e1
Justin Montgomery <tryfinsigabrt@yahoo.com>
2025-07-26 16:09:50 -0500

    Fix run count label not displaying run > 1 on first loaded screen due to timing with story level state loading

--- ui/story/run_count_label.gd
+++ ui/story/run_count_label.gd
@@ -6,6 +6,8 @@ var run_count:int = 0
 func _ready() -> void:
 	hide()
 	if SceneManager:
+		# Need to wait a frame to make sure this loads
+		await get_tree().process_frame
 		if SceneManager.story_level_state:
 			run_count = SceneManager.story_level_state.run_count
 			if run_count > 1:
